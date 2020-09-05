30102ff076f028da4811c19b7275a39997d87e5f
jamdlaw <jamdlaw@yahoo.com>
2020-09-05 12:37:55 -0700

    added request and response code

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const { format } = require('path');
+const { brotliDecompressSync } = require('zlib');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    //console.log(req.url, req.method, req.headers);
+    // this is stament basically mean at the root of the application
+    if(req.url === '/'){
+        res.write('<h1> made it to the / area </h1>');
+        return res.end();
+    }
+    
+    res.write('<h1> made it to the normal section </h1>');
+    res.end();
 });
 
 server.listen(3000);

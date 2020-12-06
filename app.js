b18893034f36b6920fe2b592376aba4e6a66bb97
jamdlaw <jamdlaw@yahoo.com>
2020-12-06 11:27:21 -0800

    added express and updated app to use express

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const http = require('http');
-const requestHandler = require('./routes')
+const express = require('express');
 
-const server = http.createServer(requestHandler);
+const app = express();
 
-server.listen(3000);
+app.use('/add-product', (req, res, next ) => {
+    res.send("<h1> Hello from product add page </h1>");
+});
+
+app.use('/', (req, res, next) => {
+    res.send("<h1> Hello from express js </h1>");
+});
+
+app.listen(3000);

d1be81b04305b6490330f2fd35c4e5b64cb9c3a4
jamdlaw <jamdlaw@yahoo.com>
2020-12-12 15:44:09 -0800

    added bady parser to project, and now app is able to cleanly parse incoming requests

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const bardyParser = require('body-parser');
 
 const app = express();
 
+app.use(bardyParser.urlencoded({extended: false}));
+
 app.use('/add-product', (req, res, next ) => {
-    res.send("<h1> Hello from product add page </h1>");
+    res.send('<form action="/product" method="POST" > <input type="text" name="tite"> <button type="submit"> submit</button> </form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    res.send("<h1> Hello from express js </h1>");
+    res.send('<h1> Welcome to node </h1>');
 });
-
 app.listen(3000);
+
